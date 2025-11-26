33206e162a1659a7650dc5777de561ce377c5e22
Reidner S.M. <reidner.medeiros@academico.unirv.edu.br>
2025-11-26 01:05:35 -0300

    Correção linha do layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js ← VERSÃO FINAL OFICIAL (use esta!)
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
+import Layout from './components/Layout';
 
 // Páginas
 import ExtrairNota from './pages/ExtrairNota';
