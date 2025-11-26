f2a021d634518c69427dbcc8f7bcd6ff7493bbac
Reidner S.M. <reidner.medeiros@academico.unirv.edu.br>
2025-11-26 00:17:35 -0300

    Correção

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const agente3_rag = require('./agente3_rag');
 const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_KEY
 );
 
 const app = express();
